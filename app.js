2243367a4ee199c9c7bc2bda3cb5fefee031377b
Graham Howard <ghoward@stevens.edu>
2018-12-14 16:04:12 -0500

    added login restriction to new thread form
    only logged-in users can start threads

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.use((error, req, res, next) => {
     return next()
 });
 
+let catalogErrMsg = ''
 app.get('/', async (req, res) => {
 
     const postsCollection = await posts();
@@ -65,15 +66,19 @@ app.get('/', async (req, res) => {
         res.render("catalog", {
             title: 'ChatSprout Thread Catalog',
             threads: threads,
-            loginuser: req.cookies.AuthCookie
+            loginuser: req.cookies.AuthCookie,
+            errmsg: catalogErrMsg
         })
     }
     else {
         res.render("catalog", {
             title: 'ChatSprout Thread Catalog',
-            threads: threads
+            threads: threads,
+            errmsg: catalogErrMsg
         })
     }
+
+    catalogErrMsg = ''
 })
 
 app.post('/newthread', async (req, res) => {
@@ -81,7 +86,12 @@ app.post('/newthread', async (req, res) => {
     const ptext = req.body.ptext
     const tuuid = uuid.v4()
 
-    if(!ptext) { 
+    if (!ptext) {    // no empty posts
+        return res.redirect('/')
+    }
+
+    if (!req.cookies.AuthCookie) {  // must be logged in to post
+        catalogErrMsg = "Only logged-in users can start threads."
         return res.redirect('/')
     }
 
@@ -216,117 +226,6 @@ app.post('/thread/:id', async (req, res) => {
 
     res.json(data)
 })
-
-// app.post('/recipes', async (req, res) => {
-//
-//     let postData = req.body
-//
-//     // validation of data
-//     if (!postData.title || typeof postData.title !== "string")
-//         return res.status(400).json({"error": "Invalid value for recipe field = title"})
-//     if (!postData.ingredients || !(postData.ingredients instanceof Array))
-//         return res.status(400).json({"error": "Invalid value for recipe field = ingredients"})
-//     if (!postData.steps || !(postData.steps instanceof Array))
-//         return res.status(400).json({"error": "Invalid value for recipe field = steps"})
-//
-//     let record = {
-//         _id: uuid.v4(),
-//         title: postData.title,
-//         ingredients: postData.ingredients,
-//         steps: postData.steps
-//     }
-//
-//     const recipesCollection = await recipes()
-//     const result = await recipesCollection.insertOne(record)
-//
-//     if (result.insertedCount === 0)
-//         return res.status(500).json({"error": "Could not insert record into database"})
-//
-//     let inserted = await recipesCollection.findOne({_id: result.insertedId})
-//
-//     res.json(inserted)
-// })
-//
-// app.put('/recipes/:id', async (req, res) => {
-//     const recipeId = req.params.id
-//
-//     let putData = req.body
-//
-//     // validation of data
-//     if (!putData.title || typeof putData.title !== "string")
-//         return res.status(400).json({"error": "Invalid value for recipe field = title"})
-//     if (!putData.ingredients || !(putData.ingredients instanceof Array))
-//         return res.status(400).json({"error": "Invalid value for recipe field = ingredients"})
-//     if (!putData.steps || !(putData.steps instanceof Array))
-//         return res.status(400).json({"error": "Invalid value for recipe field = steps"})
-//
-//     let update = {
-//         title: putData.title,
-//         ingredients: putData.ingredients,
-//         steps: putData.steps
-//     }
-//
-//     const recipesCollection = await recipes()
-//     const result = await recipesCollection.replaceOne({_id: recipeId}, update)
-//
-//     if (result.modifiedCount === 0)
-//         return res.status(500).json({"error": "Could not update recipe with id = " + recipeId})
-//
-//     let inserted = await recipesCollection.findOne({_id: recipeId})
-//
-//     res.json(inserted)
-//
-// })
-//
-// app.patch("/recipes/:id", async (req, res) => {
-//     const recipeId = req.params.id
-//
-//     let patchData = req.body
-//
-//     let update = {}
-//
-//     // validation of data
-//     if (patchData.title){
-//         if (typeof patchData.title !== "string")
-//             return res.status(400).json({"error": "Invalid value for recipe field = title"})
-//         update.title = patchData.title
-//     }
-//
-//     if (patchData.ingredients){
-//         if (!(patchData.ingredients instanceof Array))
-//             return res.status(400).json({"error": "Invalid value for recipe field = ingredients"})
-//         update.ingredients = patchData.ingredients
-//     }
-//
-//     if (patchData.steps){
-//         if (!(patchData.steps instanceof Array))
-//             return res.status(400).json({"error": "Invalid value for recipe field = steps"})
-//         update.steps = patchData.steps
-//     }
-//
-//     const recipesCollection = await recipes()
-//     const result = await recipesCollection.update({_id: recipeId}, {"$set": update})
-//
-//     if (result.modifiedCount === 0)
-//         return res.status(500).json({"error": "Could not update recipe with id = " + recipeId})
-//
-//     let inserted = await recipesCollection.findOne({_id: recipeId})
-//
-//     res.json(inserted)
-// })
-//
-//
-// app.delete('/recipes/:id', async (req, res) => {
-//     const recipeId = req.params.id
-//
-//     const recipesCollection = await recipes()
-//     let result = recipesCollection.removeOne({_id: recipeId})
-//
-//     if (result.deletedCount === 0)
-//         return res.status(500).json({"error": "Could not delete recipe with id = " + recipeId})
-//
-//     res.json({})
-// })
 
 dbinit.init()
 app.listen(3000, () => {
